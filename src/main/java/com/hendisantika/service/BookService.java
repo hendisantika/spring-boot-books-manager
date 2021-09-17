f3b3061f5c08a80018f6c2f5230151ba470c1b37
Hendi Santika <hendisantika@gmail.com>
2021-09-18 06:00:45 +0700

    #10 - Add getAll in BookService

--- src/main/java/com/hendisantika/service/BookService.java
+++ src/main/java/com/hendisantika/service/BookService.java
@@ -1,8 +1,12 @@
 package com.hendisantika.service;
 
+import com.hendisantika.model.Book;
 import com.hendisantika.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,5 +21,16 @@ import org.springframework.stereotype.Service;
 public class BookService {
     @Autowired
     private BookRepository bookRepository;
+
+    /**
+     * gets all Books from Database
+     *
+     * @return a Set containing Books
+     */
+    public Set<Book> getAll() {
+        Set<Book> bookSet = new HashSet<>();
+        bookRepository.findAll().iterator().forEachRemaining(bookSet::add);
+        return bookSet;
+    }
 
 }
