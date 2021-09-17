cf5e7c67166dbac606358b82264e67f469b20b39
Hendi Santika <hendisantika@gmail.com>
2021-09-18 06:51:45 +0700

    #55 - Add newCatForm in CategoryController

--- src/main/java/com/hendisantika/controller/CategoryController.java
+++ src/main/java/com/hendisantika/controller/CategoryController.java
@@ -1,8 +1,12 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.model.Category;
 import com.hendisantika.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,4 +22,16 @@ public class CategoryController {
 
     @Autowired
     private CategoryService categoryService;
+
+    /**
+     * Returns Form for new Category
+     * @param model     contains Category Object
+     * @return          template Form for new Category
+     */
+    @GetMapping( path = "/category/create")
+    public String newCatForm(Model model) {
+        model.addAttribute("category", new Category());
+        return "categories/new";
+    }
+
 }
